43d01ec39973b90b05bc19d8819e210fcc27ce4b
mmorandini <emmemorandini@gmail.com>
2017-07-25 16:18:41 +0100

    nav links are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PincoPallinoService} from './pincopallino.service';
+
+declare let videojs;
 
 @Component({
 	selector: 'app-root',
@@ -8,9 +10,42 @@ import { PincoPallinoService} from './pincopallino.service';
 })
 export class AppComponent {
 	title;
-	// var myPlayer = videojs('my-player');
 
 	constructor(private pincopallino: PincoPallinoService){
 		this.title = this.pincopallino.pinco;
+	}
+
+	ngOnInit(){
+		let width = document.documentElement.clientWidth;
+		let height = document.documentElement.clientHeight;
+		console.log(width);
+		var myPlayer = videojs('my-player');
+		console.log(myPlayer);
+		myPlayer.dimensions(0.9 * width, 0.8 * height);
+	}
+
+	changeSrc0(){
+		var myPlayer = videojs('my-player');
+		myPlayer.src({
+			src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+			type: 'application/x-mpegURL'
+		});
+		myPlayer.play();
+	}
+	changeSrc1(){
+		var myPlayer = videojs('my-player');
+		myPlayer.src({
+			src: 'http://www.html5videoplayer.net/videos/toystory.mp4',
+			type: 'video/mp4'
+		});
+		myPlayer.play();
+	}
+	changeSrc2(){
+		var myPlayer = videojs('my-player');
+		myPlayer.src({
+			src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+			type: 'video/mp4'
+		});
+		myPlayer.play();
 	}
 }
