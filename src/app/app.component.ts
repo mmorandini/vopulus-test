dc8c6fd3c789f95ebe71d6b0f88a78955d63b641
mmorandini <emmemorandini@gmail.com>
2017-07-25 23:14:44 +0100

    added player component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PincoPallinoService} from './pincopallino.service';
+import {Headers} from '@angular/http';
 
 declare let videojs;
+
+
 
 @Component({
 	selector: 'app-root',
@@ -15,37 +18,45 @@ export class AppComponent {
 		this.title = this.pincopallino.pinco;
 	}
 
+	firstVideoIsOn = false;
+	secondVideoIsOn = false;
+	thirdVideoIsOn = false;
+
 	ngOnInit(){
 		let width = document.documentElement.clientWidth;
 		let height = document.documentElement.clientHeight;
-		console.log(width);
-		var myPlayer = videojs('my-player');
-		console.log(myPlayer);
-		myPlayer.dimensions(0.9 * width, 0.8 * height);
+		let myPlayer = videojs('my-player');
+		// myPlayer.dimensions(0.9 * width, 0.8 * height);
 	}
 
-	changeSrc0(){
-		var myPlayer = videojs('my-player');
+	changeSrc(){
+		let sources = ['https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8','http://www.html5videoplayer.net/videos/toystory.mp4','http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4']
+		let myPlayer = videojs('my-player');
+		let src;
+		let type;
+
+		if (this.firstVideoIsOn === true){
+			src  = sources[0];
+			type = 'application/x-mpegURL';
+		} else if (this.secondVideoIsOn === true){
+			src = sources[1];
+			type = 'video/mp4';
+		} else if (this.thirdVideoIsOn === true){
+			src = sources[2];
+			type = 'video/mp4';
+		} else return;
 		myPlayer.src({
-			src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-			type: 'application/x-mpegURL'
+			src: src,
+			type: type
 		});
 		myPlayer.play();
+		this.firstVideoIsOn = false;
+		this.secondVideoIsOn = false;
+		this.thirdVideoIsOn = false;
+		console.log(myPlayer.aspectRatio());
 	}
-	changeSrc1(){
-		var myPlayer = videojs('my-player');
-		myPlayer.src({
-			src: 'http://www.html5videoplayer.net/videos/toystory.mp4',
-			type: 'video/mp4'
-		});
-		myPlayer.play();
-	}
-	changeSrc2(){
-		var myPlayer = videojs('my-player');
-		myPlayer.src({
-			src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-			type: 'video/mp4'
-		});
-		myPlayer.play();
-	}
+	
+
+
+	
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { PincoPallinoService } from './pincopallino.service';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
